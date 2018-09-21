126847584c7112f51e9394138b9790e9d4efb1da
Nick Sinclair <nick.sinclair98@gmail.com>
2018-09-21 00:13:58 -0400

    Added Game Files
    Added the game in the state I had made it in locally

---
+++ Basic Runner/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    bool gameEnded = false;
+
+    public float restartDelay = 1f;
+
+    public void EndGame()
+    {
+        if (!gameEnded)
+        {
+            gameEnded = true;
+            Debug.Log("GAME OVER");
+            Invoke("Restart", restartDelay);
+            // Restart Game
+        }
+        
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
